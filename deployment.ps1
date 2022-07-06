7682a13371b99c76f0653ceb670acb797055b131
ElManchacho <pld.pro@hotmail.com>
2022-07-06 10:45:27 +0200

    init

---
+++ deployment.ps1
@@ -0,0 +1,15 @@
+# unzip
+
+# installer python
+
+# script python pour demander des paramètres pour config le .yaml + lien vers cloud et/ou kibana
+
+# remplacer le .yaml
+
+# script d'instanciation du service filebeat blablabla.ps1
+
+# start service
+
+# générer des logs basiques pour faire un tests de création d'index
+
+# renvoyer vers le cloud ou kibana
