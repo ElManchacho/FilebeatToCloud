ef947f78c9d2cdd4cb890dd5d1204254093bce63
Paul <pld.pro@hotmail.com>
2022-07-29 17:07:13 +0200

    logs format versatility for export enabled

--- filebeatScripts/csvParser.js
+++ filebeatScripts/csvParser.js
@@ -1,4 +1,14 @@
 function process(event) {
-    event.Put("filebeatScriptMessage", event.Get())
-    return event
+
+    var message = event.Get("message");
+    
+    event.Put("eventMessage", message);
+
+    var slicedText = message.split(';');
+
+    slicedText.forEach(function(field, index) {
+        event.Put('parsed.'+index, field);
+    });
+    
+    return event;
 }
