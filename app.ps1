f0c5cac28e8f21605fc659e72dc2e08a58162e20
AL1062886 <paul.leroyducardonnoy@cgi.com>
2023-07-28 09:33:43 +0200

    added asking for root privilieges to start

--- app.ps1
+++ app.ps1
@@ -1,3 +1,8 @@
-$filebeatVersion = "filebeat-8.8.2-windows-x86_64"
+if(!([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] 'Administrator')) {
+    
+    Start-Process -FilePath PowerShell.exe -windowstyle hidden -Verb Runas -ArgumentList "-File `"$($MyInvocation.MyCommand.Path)`"  `"$($MyInvocation.MyCommand.UnboundArguments)`""
+  
+    $filebeatVersion = "filebeat-8.8.2-windows-x86_64"
 
-py "./py-scripts/main.py" -p $filebeatVersion -Wait
+    py "./py-scripts/main.py" -p $filebeatVersion -Wait
+}
